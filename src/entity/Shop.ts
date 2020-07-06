0c7b3d98d54692f4aa12c845dc244a554397a78c
Carlos Petit <carlos.petit@sr-consultores.com>
2020-07-06 19:09:19 -0400

    add shop model

---
+++ src/entity/Shop.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { CompanyInterface } from "./Company";
+
+export interface ShopInterface extends Document {
+    address: string;
+    phone?: string;
+    company: CompanyInterface['_id'];
+    number?: number;
+}
+
+
+/*
+    Los perfiles que actualmente manejamos son 
+    2: Picker
+    4: Moto o Delivery
+ */
+
+const ShopSchema: Schema = new Schema({
+    number: { type: String, required: false },
+    address: { type: String, required: true },
+    phone: { type: String, required: false },
+    company: { type: Schema.Types.ObjectId, required: true, ref: "Companies" }
+});
+
+const Shop = mongoose.model<ShopInterface>("Shop", ShopSchema);
+export default Shop;
