0a862da57485a7cc82847e6708e7eeaaaeecfe2b
Carlos Petit <carlos.petit@sr-consultores.com>
2020-07-06 19:10:42 -0400

    Add order controller

--- src/controller/OrdersController.ts
+++ src/controller/OrdersController.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
 const jwt = require('jsonwebtoken');
-// import { User } from "../entity/User";
-
-
-
+import Order from "../entity/Orders";
+const { initDB, insertDB, insertManyDB } = require("../config/db")
 
 
 export class OrdersController {
@@ -30,12 +29,47 @@ export class OrdersController {
     return null
   }
 
+  /*
+    Metodo que recibe un array de ordenes para guardarlas en la base de datos
+ */
   async save(request: Request, response: Response, next: NextFunction, app: any) {
-    response.json({
-      mensaje: 'orden actualizada correctamente',
-      data: "s",
-      success: true
-    });
+    try {
+      let orders: Array<any>;
+      orders = request.body.orders
+      let _orders: Array<any> = []
+      orders.map((order, index) => {
+        let _order = {
+          uid: mongoose.Types.ObjectId(request.body.uid),
+          id: order.id,
+          orderNumber: order.orderNumber,
+          products: order.products,
+          client: order.client,
+          date: order.date,
+          startPickingDate: new Date(),
+          endPickingDate: new Date(),
+          starDeliveryDate: new Date(),
+          endDeliveryDate: new Date(),
+          realdatedelivery: new Date(),
+          pickerWorkShift: new Date()
+        }
+        _orders.push(_order)
+      })
+
+      await insertManyDB(Order, _orders)
+
+
+      response.json({
+        mensaje: 'orden creada exitosamente',
+        data: "s",
+        success: true
+      });
+    } catch (error) {
+      console.log(error)
+      response.json({
+        mensaje: error.message,
+        success: false
+      });
+    }
   }
 
   async ordersToDelivery(request: Request, response: Response, next: NextFunction, app: any) {
