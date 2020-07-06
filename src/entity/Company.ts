afe31294674ee7312d13eb7b7407d30480a9c83b
Carlos Petit <carlos.petit@sr-consultores.com>
2020-07-06 19:10:59 -0400

    update company model

---
+++ src/entity/Company.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface CompanyInterface extends Document {
+    name: string;
+    rut: string;
+    email: string,
+    phone: string
+}
+
+
+/*
+    Los perfiles que actualmente manejamos son 
+    2: Picker
+    4: Moto o Delivery
+ */
+
+const CompanySchema: Schema = new Schema({
+    rut: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true }
+});
+
+const Company = mongoose.model<CompanyInterface>("Company", CompanySchema);
+export default Company;
