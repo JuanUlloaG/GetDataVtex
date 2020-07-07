bdcd6fb0d085b937d9d258ed9d225a8f1f6fbef7
Carlos Petit <carlos.petit@sr-consultores.com>
2020-07-07 10:50:44 -0400

    update gitignore

--- dist/types/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export interface IGetUserAuthInfoRequest extends Request, Response {
-//     decoded: string
-//     headers: string
-// }
